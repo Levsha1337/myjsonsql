58c918b45b11a878e4226dcd6b0d571a25fd09db
Levsha1337 <dimentor.2000@mail.ru>
2020-12-02 00:12:30 +0300

    import instead require

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mysql2 = require('mysql2/promise');
+import mysql2 from 'mysql2/promise';
 
 class TableAdapter {
     connection = null;
@@ -111,4 +111,4 @@ class MySQLJSON {
     }
 }
 
-module.exports = MySQLJSON;
+export default MySQLJSON;
